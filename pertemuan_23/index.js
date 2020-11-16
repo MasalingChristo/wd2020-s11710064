42d44751eb857285a67b0ee52d3c8e08a226e4b4
MasalingChristo <s11710064@student.unklab.ac.id>
2020-11-15 20:05:41 -0800

    push exercise 7 variabel and operator

---
+++ pertemuan_23/index.js
@@ -0,0 +1,74 @@
+// console.log("hallo javascript");
+
+    //#variabel= let & const
+//const variabel bersifat fix
+
+// let umur=25;
+// let sudahmenikah = true;
+
+// let nama="John Doe";
+
+// console.log(umur);
+// console.log(nama);
+
+// let alamat;
+// console.log(alamat)
+
+// const tahunlahir=2010;
+// console.log(tahunlahir);
+
+
+
+    //#operator aritmatic
+// let angka1=10;
+// let angka2=20;
+// let hasil= angka1 + angka2;
+// console.log (hasil);
+
+// console.log(angka1 + angka2);
+
+// let umur = 30;
+// let namaDepan= "John";
+// let namaBelakang="Doe";
+// console.log("Nama saya " + namaDepan +" "+ namaBelakang + ". umur Saya adalah" + umur);
+
+    //#operator Perbandingan
+
+// let angka1= 10;
+// let angka2= 9;
+
+//let hasil= angka1 == angka2;
+// console.log(angka1 === angka2);
+
+    //ternary operator
+// let angka = 5;
+// let hasil= angka < 0 ? "Negatif" : "Positif";
+// console.log(hasil);
+
+    //Operator Logika
+// let kondisi1 = 1 < 2;
+// let kondisi2 = true;
+
+// console.log (kondisi1 & kondisi2);
+
+// console.log(kondisi1 || kondisi2);
+
+// console.log(!kondisi1 )
+
+
+
+//meminta input user
+
+// let angka = prompt ("masukkan angka : ");
+
+
+let berat = prompt ("Masukkan Berat Badan anda :")
+let tinggi = prompt ("Masukkan tinggi Badan anda  : ");
+
+console.log ("Berat Badan anda adalah :" + berat + " kg");
+console.log ("tinggi badan anda adalah :" + tinggi +" cm");
+
+let BMI= berat /(tinggi * tinggi);
+
+console.log("Body Mass Index anda adalah :" + BMI);
+
